7f205649dbbf818f39c73fd8d25d6acda08f5bc4
Lilian Carvalho Santos Ribeiro <kleidk74@gmail.com>
2023-05-02 14:45:33 -0300

    Sintaxe geral

---
+++ 01-sintaxe-geral.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sintaxe geral do PHP</title>
+</head>
+<body>
+    <h1>Experimentando PHP</h1>
+    <hr>
+
+<?php
+echo "Olá PHP!";
+echo "teste";
+$escola = "senac";
+
+echo $escola;
+
+// CTRL + ; comentários de uma linha
+/* ALT + SHIFT + A comentários de múltiplas linhas */
+?>
+
+</body>
+</html>
